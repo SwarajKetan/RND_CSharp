845be17d06101fa921e4bfef5dddda08b24739a1
Swaraj <swarajksantra@gmail.com>
2020-04-12 20:22:31 +0530

    Easy program runner without unit tests

---
+++ RND_CS/ProgramRunner/MyDemoProgram1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramRunner
+{
+    [RunThis]
+    public class MyDemoProgram1 : Runnable
+    {
+        public override void Run(string[] args)
+        {
+            // Write ur program here
+        }
+    }
+}
---
+++ RND_CS/ProgramRunner/MyDemoProgram2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramRunner
+{
+    //[RunThis]
+    public class MyDemoProgram2 : Runnable
+    {
+        public override void Run(string[] args)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
