a0c87f76224e076d96a06a2b4cffd2f7461a21dd
Swaraj Ketan Santra <swarajksantra@gmail.com>
2021-04-24 12:22:25 +0000

    saveing changes

--- RND_CS/ConsoleApp1/Program.cs
+++ RND_CS/ConsoleApp1/Program.cs
@@ -6,29 +6,61 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    using System.Collections.Generic;
+    using System.IO;
+    using System.IO.Pipes;
+
     class Program
     {
-        static async Task Main(string[] args)
+        public interface ILogHandler : IDisposable
         {
-            try
+            void Log(string message);
+        }
+
+        public class RemoteLogHandler : ILogHandler
+        {
+            readonly NamedPipeServerStream pipeServer = new NamedPipeServerStream("abcpipe", PipeDirection.Out);
+            readonly StreamWriter sw = null;
+
+            public RemoteLogHandler()
             {
-                Class1 c = new Class1();
-                Stopwatch sw = new Stopwatch();
-                sw.Start();
-                bool res = await c.CallAsync<bool>(DoSomething, 3000);
-                sw.Stop();
-                Console.WriteLine($"elapsed {sw.ElapsedMilliseconds}");
+                pipeServer.WaitForConnection();
+                sw = new StreamWriter(pipeServer) { AutoFlush = true };
             }
-            catch (Exception ex)
+            public void Log(string message)
             {
-                Console.WriteLine(ex.Message);
+                _ = sw.WriteLineAsync(message);
+            }
+            public void Dispose()
+            {
+                sw.Dispose();
+                pipeServer.Dispose();
             }
         }
 
-        static bool DoSomething()
+
+        static List<ILogHandler> providers = new List<ILogHandler>();
+
+        static void Main(string[] args)
         {
-            Thread.Sleep(5000);
-            return true;
+
+            providers.Add(new RemoteLogHandler());
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    providers.ForEach(p => p.Log(DateTime.Now.ToString()));
+                    Thread.Sleep(1000);
+                }
+            });
+
+
+            Console.ReadLine();
+            Console.WriteLine("Server exit");
+            providers.ForEach(p=> p.Dispose());
+            Console.ReadLine();
         }
+
     }
 }
---
+++ RND_CS/WindowsService1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService1
+{
+    public class Class1 : System.ServiceProcess.ServiceBase
+    {
+        protected override void OnStart(string[] args)
+        {
+            Console.WriteLine("hello started");
+            //base.OnStart(args);
+        }
+
+        protected override void OnStop()
+        {
+            //
+            Console.WriteLine("stopped");
+        }
+    }
+}
